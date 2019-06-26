ad4794691f2702621fd070882f412802b8044aa3
Matthew Heavner <mrh9ud@virginia.edu>
2019-06-26 23:17:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function happyHolidayTo(holiday, firstName) {
   return `Happy ${holiday}, ${firstName}!`
 }
 
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
+
